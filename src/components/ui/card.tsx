46786ad0e12d48bb5ae52cb40825eacdbf6da136
Yuri Schulz <schulzyuri@gmail.com>
2025-09-05 17:49:47 -0300

    refactor: Unificar estilos do título do cartão e aplicar classe de estilo no CSS

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -38,14 +38,7 @@ const CardTitle = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
-    className={cn(
-      'text-lg font-semibold leading-none tracking-tight',
-      className
-    )}
-    {...props}
-  />
+  <h3 ref={ref} className={cn('card', className)} {...props} />
 ));
 CardTitle.displayName = 'CardTitle';
 
